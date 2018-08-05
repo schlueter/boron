a3cbf0f5ca3bc30339b80cd4b189f92eb25c8a7e
Schlueter <b@schlueter.blue>
2018-08-05 01:01:09 -0400

    Add the init file for the boron package

---
+++ boron/__init__.py
@@ -0,0 +1,31 @@
+# Copyright 2017 Brandon Schlueter
+# This file is part of boron.
+#
+# Boron is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import os
+import sys
+
+from boron.wsgi import WSGIApp
+
+assert WSGIApp
+
+description = """
+    Boron is a framework for simple http applications. I did not intend to write it, but
+    created it while writing ghh as a way to isolate the http logic from the business
+    logic of that application.
+    """
+
+version = '2018.08.04-alpha1'
+default_config_file = os.path.join(sys.prefix, 'etc/boron/config.yml')
